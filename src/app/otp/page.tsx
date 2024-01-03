712e0c42c470e73ab1a2fa76865eab0d4bc3ffd5
Mohammad Mahdi Talachi <mohammadmahditalachi@gmail.com>
2024-01-04 02:53:14 +0330

    fix

--- src/app/otp/page.tsx
+++ src/app/otp/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import OtpInput from "react-otp-input";
 import "./page.css";
+import LinkHero from "@/components/shared/LinkHero";
 
 const page = () => {
   const [otp, setOtp] = useState("");
@@ -11,18 +12,22 @@ const page = () => {
     <div
       dir="ltr"
       id="otp-container"
-      className="flex flex-col space-y-3 justify-center font-bold text-xl h-screen max-w-sm mx-auto"
+      className="flex flex-col space-y-8 justify-center font-bold text-xl h-screen max-w-sm mx-auto"
     >
+      <div className="text-lg text-right">
+        <p>:کد تایید را وارد کنید</p>
+      </div>
       <OtpInput
         value={otp}
         onChange={setOtp}
         numInputs={5}
         inputStyle={
-          "bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          "inputStyle bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
         }
-        renderSeparator={<span className="m-2"></span>}
+        renderSeparator={<span className="m-3"></span>}
         renderInput={(props) => <input {...props} />}
       />
+      <LinkHero title="ادامه" href="#" className="w-full" />
     </div>
   );
 };
--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -4,9 +4,9 @@ const Logo = () => {
   return (
     <span className="font-bold text-3xl">
       <span className="text-blue-700">جا</span>
-      <span className="text-white">پارک</span>
+      <span className="text-blue-700 dark:text-white">پارک</span>
       <span className="text-blue-700">با</span>
-      <span className="text-white">ما</span>
+      <span className="text-blue-700 dark:text-white">ما</span>
     </span>
   );
 };
