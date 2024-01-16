a05ba57f7fe74aa18e894c1ce73d0b6f1af1a66f
Mohammad Mahdi Talachi <mohammadmahditalachi@gmail.com>
2024-01-17 00:07:38 +0330

    fix

--- src/app/(map)/finding/page.tsx
+++ src/app/(map)/finding/page.tsx
@@ -6,15 +6,15 @@ import Waiting from "@/components/shared/Waiting";
 
 const page = () => {
   return (
-    <div className="px-5 pb-16 h-screen flex flex-col justify-end items-center space-y-20">
-      <div className="mb-24" role="status" dir="ltr">
+    <div className="px-5 h-screen flex flex-col justify-center items-center space-y-20">
+      {/* <div className="mb-24" role="status" dir="ltr">
         <div className="relative h-16 w-16">
           <div className="item-loader"></div>
           <div className="item-loader"></div>
           <div className="item-loader"></div>
           <div className="item-loader"></div>
         </div>
-      </div>
+      </div> */}
       <TicTacToe />
       <div className="inline-flex justify-center  items-center">
         <p>در حال یافتن جای پارک </p>
